6e26b1a8a63deac2f0b6a9f166626b52cba25224
Tejraj0319 <rajeshpatil0319@gmail.com>
2026-05-06 14:13:59 +0530

    fix: Correct syntax for BASE_URL declaration in adminAPI.js

--- admin-dashboard/src/features/admin/adminAPI.js
+++ admin-dashboard/src/features/admin/adminAPI.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 // const BASE_URL = "http://localhost:5000/api/v1";
-const BASE_URL = import.meta.env.VITE_API_URL,
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 const getToken = () => { return localStorage.getItem("token") }
 
 const config = () => ({
     headers: {
-        Authorization: `Bearer ${getToken()}`
+        Authorization: `Bearer ${getToken()}`   
     }
 })
 
